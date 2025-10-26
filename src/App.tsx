9d2230d7d76a448614a94baaa4b82d4ef1afe82f
LuchoUrtubey <luisgerardourtubey@gmail.com>
2025-10-26 01:14:58 -0300

    feat: mensaje de construccion

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+const App = () => {
+  return (
+    <>
+    <h1><span className="emoji"></span>🚧 Under Construction</h1>
+    <p>Meanwhile, you can check out my <a href="https://github.com/LuchoUrtubey" target="_blank">GitHub profile</a>.</p>
+    </>
+  )
+}
+export default App
